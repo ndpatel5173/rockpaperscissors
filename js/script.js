a16d1b90686f74b5da1638a8fa1da08a5be7029a
ndpatel5173 <npatel8745.student@hsfi.us>
2019-11-18 23:05:49 +0000

    I made the conditonals for computerchoice.

--- js/script.js
+++ js/script.js
@@ -5,8 +5,25 @@
 //GLOBAL VARIABLES
 /* global $ */
 $("#shoot").click(function(){
-let answer = $("#input").val()
-$("#userChoice").text(answer)
+    let answer = $("#input").val();
+    $("#userChoice").text(answer);
+
+//else if conditionals for computer
+    var num = Math.random();
+       console.log(num);
+    if (num >= 0.60){
+        $("#computerChoice").text("rock");
+        //console.log("rock");
+    }
+    else if(num < 0.60 && num >= 0.30){
+         $("#computerChoice").text("paper");
+        //console.log("paper");
+    }
+    else if(num < 0.30 && num >=0){
+         $("#computerChoice").text("scissors");
+        //console.log("scissors");
+    }
+//else if statements for comparing
 });
 
 
