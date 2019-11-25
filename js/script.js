ba9118cf540112d12642fcc28a23c9ca3028ebfa
ndpatel5173 <npatel8745.student@hsfi.us>
2019-11-25 23:03:22 +0000

    11/25/2019 I failed in comparing the values of user and computer.

--- js/script.js
+++ js/script.js
@@ -12,18 +12,33 @@ $("#shoot").click(function(){
     var num = Math.random();
        console.log(num);
     if (num >= 0.60){
-        $("#computerChoice").text("rock");
-        //console.log("rock");
+         $("#computerChoice").text("rock");
+        var computerChoice1 = rock; 
     }
     else if(num < 0.60 && num >= 0.30){
          $("#computerChoice").text("paper");
-        //console.log("paper");
+        var computerChoice2 = paper;
     }
     else if(num < 0.30 && num >=0){
          $("#computerChoice").text("scissors");
-        //console.log("scissors");
+        var computerChoice3 = scissors;
     }
 //else if statements for comparing
+    // userChoice = answer      computerChoice = answer2
+    let answer1 = rock
+    let answer2 = paper
+    let answer3 = scissors
+    //let answer2 = $("#computerChoice").val();
+
+    if(answer1 === computerChoice1){
+        $("#result").text("TIE!!!");
+    }
+    else if(answer2 === computerChoice2){
+        $("#result").text("TIE!!!");
+    }
+    else if(answer3 === computerChoice3){
+        $("#result").text("TIE!!!");
+    }
 });
 
 
